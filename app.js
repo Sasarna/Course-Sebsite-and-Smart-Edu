233473205786c9aa8f4740fc17e022ef0d5a3295
yakup <yakuphad200@gmail.com>
2024-12-05 22:13:31 +0300

    basic route management and file editing

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 
+const ejs = require('ejs');
+
 const PORT = 3000;
 
+//* Template Engine
+app.set('view engine' , 'ejs');
+
+//* Middleware
+app.use(express.static('public'));
+
+//* Routes
+
 app.get('/' , (req , res) => {
-    res.status(200).send('İndex sayfası')
+    res.status(200).render('index' , {
+        page_name: "index"
+    });
 })
+
+app.get('/about' , (req , res) => {
+    res.status(200).render('about' , {
+        page_name: "about"
+    });
+});
 
 app.listen(PORT , () => {
     console.log(`App started on port : ${PORT}`);
